228debb41cd2c82e52eff1bdb188ebfa8b9301ad
seungyeon <ssyeon613@gmail.com>
2023-11-01 13:57:26 +0900

    Create Component, Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+        <nav>
+            {/*<a href="/">Home</a>*/}
+            {/*아래와 같음*/}
+            <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+      {/*<Home />*/}
     </div>
   );
 }
